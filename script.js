3ea0e870a9417b0bbe41b00677ff90ea4a38aa97
Jennifer Wong <jennifer.design@yahoo.com>
2020-08-14 01:53:40 -0500

    Local Storage Updates

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 /* ------------------------------- GLOBAL VARIABLES ------------------------------- */
 
 let $listCurrentDay = $("#currentDay");
@@ -9,13 +7,7 @@ $listCurrentDay.text(moment().format("dddd, MMMM Do"));
 // Current Time (real-time)
 let now = parseInt(moment().format("k"));
 
-// All Text Area with data attribute
-let allTextArea = $("textarea");
-// allTextArea.text("Enter description here");
-
-let dataTextArea = allTextArea.attr("data");
-// console.log(dataTextArea);
-
+/* Text Area DOM Elements to Affect Color */
 let time8 = $("#8"); //0    8
 let time9 = $("#9"); //1    9
 let time10 = $("#10"); //2    10
@@ -27,33 +19,51 @@ let time15 = $("#15"); //7    3
 let time16 = $("#16"); //8    4
 let time17 = $("#17"); //9    5
 
-let timeslot = [ time8, time9, time10, time11, time12, time13, time14, time15, time16, time17 ];
 
 /* ------------------------------- FUNCTIONS ------------------------------- */
 
-function getTime() {
-// Get the time
-// If time is == to the row ID, then change color to red
-// If time is > less than the row ID, then change color to green
-};
+/* Text Area DOM Elements placed into Array to match with "now" */
+let timeslot = [ time8, time9, time10, time11, time12, time13, time14, time15, time16, time17 ];
 
+
+/* Global Variables #2 */
+
+// All Text Area with data attribute
+let $allTextBox = $("textarea");
+// allTextArea.text("Enter description here");
+
+let $allButton = $(".saveBtn");
+// $allButton.text("hi");
+
+$(document).ready(function() {
+
+
+$(document).on("click", ".saveBtn", storeInput);
+
+let btnEl = [];
+// console.log(btnEl);
+
+function storeInput(){
+
+    let inputvalue = $(this).attr("data-number");
+    let buttonID = $("#" + inputvalue).val().trim();
+    localStorage.setItem("todo", JSON.stringify(buttonID));
+
+}
+
+
+/* ------------------------------- GLOBAL FUNCTION ------------------------------- */
 
 function colorRowTime(){
-// Present = .present
-// Future = .future
-for (let i = 0; i < 10; i++){
-  if (i+8 === now){
-    timeslot[i].addClass("present")
-  }
-  if (i+8 > now) {
-    timeslot[i].addClass("future");
-    }
-}
-};
-
-
-function storeUserInfo(){
-
-};
-
+      for (let i = 0; i < 10; i++){
+        if (i+8 === now){
+          timeslot[i].addClass("present")
+        }
+        if (i+8 > now) {
+          timeslot[i].addClass("future");
+          }
+      }
+      };
 colorRowTime();
+
+});
